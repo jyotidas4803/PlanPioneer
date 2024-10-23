d5d8bdf7e058c6a51ee15c4b2b4194807976fe9f
Jyotiii Das <163004395+jyotidas4803@users.noreply.github.com>
2024-10-23 08:00:31 +0000

    user interface

--- src/main.js
+++ src/main.js
@@ -1,1 +1,4 @@
 import "./index.css";
+
+const showDateEL = document.querySelector(".showDate");
+showDateEL.textContent=new Date().getFullYear()
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: [  "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
+    fontFamily:{
+      "figtree":"Figtree", "sans-serif"
+    },
     extend: {},
   },
   plugins: [require('daisyui'),],
