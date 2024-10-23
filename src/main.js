f3ae14e04d1b41a13e842c68c58c4d015ab30b74
Jyotiii Das <163004395+jyotidas4803@users.noreply.github.com>
2024-10-23 15:00:26 +0000

    some crazy effect

--- src/main.js
+++ src/main.js
@@ -2,3 +2,14 @@ import "./index.css";
 
 const showDateEL = document.querySelector(".showDate");
 showDateEL.textContent=new Date().getFullYear()
+
+
+const inputEl = document.querySelector(".input");
+
+function handleInput(event) {
+    if (event.key === "Enter") {
+        console.log(inputEl.value);  // Logs the entire input once "Enter" is pressed
+    }
+}
+
+inputEl.addEventListener('keyup', handleInput);
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ export default {
   content: [  "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    fontFamily:{
-      "figtree":"Figtree", "sans-serif"
+    fontFamily: {
+      figtree: ["Figtree", "sans-serif"],
     },
     extend: {},
   },
