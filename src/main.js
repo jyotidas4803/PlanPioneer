10719789550f41b95597792b0a3481b3ee86856f
Jyotiii Das <163004395+jyotidas4803@users.noreply.github.com>
2024-10-28 07:33:34 +0000

    create new task for user input

--- src/main.js
+++ src/main.js
@@ -1,15 +1,43 @@
 import "./index.css";
 
+
+import { titleCase } from "./utils";
+
+// __DOM manipulation
 const showDateEL = document.querySelector(".showDate");
+const inputEl=document.querySelector("[data-user-value]")
+const formEl=document.querySelector("[data-form]");
+
+// vars
+
+const tasks=[];
+
+// current Year
 showDateEL.textContent=new Date().getFullYear()
 
 
-const inputEl = document.querySelector(".input");
+// const inputEl = document.querySelector(".input");
 
-function handleInput(event) {
-    if (event.key === "Enter") {
-        console.log(inputEl.value);  // Logs the entire input once "Enter" is pressed
-    }
-}
+// function handleInput(event) {
+//     if (event.key === "Enter") {
+//         console.log(inputEl.value);  // Logs the entire input once "Enter" is pressed
+//     }
+// }
 
-inputEl.addEventListener('keyup', handleInput);
+// inputEl.addEventListener('keyup', handleInput);
+
+
+formEl.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    if(!inputEl.value) return;
+const newTask ={
+    text:titleCase(inputEl.value),
+    isCompleted:false,
+    id: tasks.length
+};
+
+tasks.unshift(newTask)
+
+    console.log(tasks);
+    inputEl.value= " "
+})
---
+++ src/utils.js
@@ -0,0 +1,8 @@
+export function titleCase(str){
+    return str
+    .trim()
+    .split(" ")
+    .map(w=> w[0].toUpperCase() + w.slice(1).toLowerCase())
+    .join(" ")
+
+}
